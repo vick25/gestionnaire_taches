cf808736420fce18295952608d43de7f5c6d52c8
Victor Kadiata <vickadiata@gmail.com>
2024-11-26 21:06:18 +0100

    Ajoute une catégorie à chaque tâche

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+const categorieEnum = Object.freeze({
+    Travail: "Travail",
+    Personnel: "Personnel",
+    Urgent: "Urgent"
+});
+
 const creerTache = () => {
     return {
         nom: "",
-        dateEcheance: "",
+        dateEcheance: null,
         priorite: "",
         estTerminee: "",
         categorie: "",
-        rappels: "",
+        rappels: [],
         defineTache: function (nom, dateEcheance, priorite, estTerminee, categorie, rappels) {
             this.nom = nom;
             this.dateEcheance = dateEcheance;
@@ -16,3 +22,7 @@ const creerTache = () => {
         }
     }
 }
+
+const tache1 = creerTache();
+tache1.defineTache('Tache1', new Date().toLocaleDateString(), "2", true, categorieEnum.Urgent, []);
+console.log(creerTache());
