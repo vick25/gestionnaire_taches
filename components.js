eab2fbfc55da6374cc4207498e886b06b7dc711b
Victor Kadiata <vickadiata@gmail.com>
2024-12-03 19:22:36 +0100

    Refactoring the code

--- components.js
+++ components.js
@@ -1,7 +1,7 @@
 const TacheCardComponent = (tache) => {
     return `<li key=${tache.tacheID} class="taches__item">
         <div class="taches__item-wrapper">
-            <div class=${tache.estTerminee ? "text-decoration-line-through taches__item_head" : "taches__item_head"}>
+            <div class=${tache.estTerminee ? "text-decoration-line-through taches__item__body" : "taches__item__body"}>
                 <h3>${tache.nom}</h3>
                 <p><b>Catégorie</b> : ${tache.categorie}</p>
                 <p><b>Priorité</b> : ${tache.priorite}</p>
